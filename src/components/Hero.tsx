c8a43766e19cd6559f93bc225e845a5938874650
sadiqrehman <sadiqrahman192@gmail.com>
2025-05-22 16:37:23 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,6 @@ import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import ThreeDAnimation from "./ThreeDAnimation";
-import hero1 from '../public/cv.pdf'; 
 
 const Hero = () => {
   const scrollToSection = (sectionId: string) => {
@@ -119,20 +118,6 @@ const Hero = () => {
               onClick={() => scrollToSection("contact")}
             >
               Contact Me
-            </Button>
-            <Button
-              size="lg"
-              className="px-6"
-              onClick={() => {
-                const link = document.createElement("a");
-                link.href = "/cv.pdf"; // Path to the PDF in public folder
-                link.download = "cv.pdf";
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link);
-              }}
-            >
-              Download CV
             </Button>
           </motion.div>
         </div>
